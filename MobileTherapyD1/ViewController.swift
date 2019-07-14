39840355830f761c831837ed2438556b62e25480
Giannte Page <Gianntepage@gmail.com>
2019-07-14 17:40:44 -0400

    test

--- MobileTherapyD1/ViewController.swift
+++ MobileTherapyD1/ViewController.swift
@@ -13,9 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-Print ("Hello World")
     
+print ("Hello World")
+        //wassup
+        
+    }
     
 
 }
